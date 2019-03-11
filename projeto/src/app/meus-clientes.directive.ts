d7f8848fd7a7db017f65510d547485395b759c85
Tiago.Weege <tiago.weege@senior.com.br>
2019-03-11 13:14:18 -0300

    #28

--- projeto/src/app/meus-clientes.directive.ts
+++ projeto/src/app/meus-clientes.directive.ts
@@ -1,12 +1,31 @@
-import { Directive, ElementRef, HostListener } from '@angular/core';
+import { Directive, ElementRef, HostListener, Input } from '@angular/core';
+import { Cliente } from './cliente';
 
 @Directive({
   selector: '[appMeusClientes]'
 })
 export class MeusClientesDirective {
 
+  private _appMeusClientes: Cliente;
+
   constructor(private el:ElementRef) { 
     this.el.nativeElement.innerHTML += 'conteudo adicionado'
+  }
+
+  get meuCliente(){
+    return this._appMeusClientes;
+  }
+
+  @Input()
+  appMeusClientes: Cliente;
+
+  @HostListener('click')
+  onClick(){
+    alert(this.appMeusClientes)
+  }
+
+  mudarCorCliente(){
+    this.el.nativeElement.style.color = this.appMeusClientes.value > 30 ? 'green': 'red';
   }
 
 }
