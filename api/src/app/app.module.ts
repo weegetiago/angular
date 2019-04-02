933aa3be3700d24eb3aa7f7169ebe08917f1577f
DESKTOP-UFSS42T\tiago <weegetiago@gmail.com>
2019-04-01 21:16:15 -0300

    #41 bug no import { Http } from '@angular/http';

--- api/src/app/app.module.ts
+++ api/src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import {HttpModule} from '@angular/http';
+import { HttpClientModule } from '@angular/common/http'; 
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -15,7 +15,7 @@ import { CepService } from './cep.service';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    HttpModule
+    HttpClientModule
   ],
   providers: [CepService],
   bootstrap: [AppComponent]
